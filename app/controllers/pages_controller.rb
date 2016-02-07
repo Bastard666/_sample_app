0c8e5259270be07755f3e59338d901beaba405b9
Loic Jouhannet <loic.jouhannet@gmail.com>
2016-02-07 21:20:13 +0100

    Using layout and helper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,20 @@
 class PagesController < ApplicationController
+  # Controller for home page
   def home
   	@title = 'Accueil'
   end
 
+  # Controller for contact page
   def contact
   	@title = 'Contact'
   end
 
+  # Controller for about page
   def about
   	@title = 'A propos'
   end
 
+  # Controller for help page
   def help
   	@title = 'Aide'
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	# Return the title of the page
+	def title
+		base_titre = "Simple App du Tutoriel Ruby on Rails"
+		@title.nil? ? base_titre : "#{base_titre} | #{@title}"
+	end
 end
